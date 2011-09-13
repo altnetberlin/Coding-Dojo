0b264a073b57f090bed1a311294446acc724c966
Mike Bild <mike@mikebild.com>
2011-09-13 20:06:44 +0200

    init

---
+++ Kata Tennis 13.09.2011 Hase/source/when_create_a_new_tennis_game.cs
@@ -0,0 +1,42 @@
+﻿using Machine.Specifications;
+
+namespace KataTennis
+{
+
+    public class Game
+    {
+        public void Start(GameScore gameScore)
+        {
+            CurrentScore = gameScore;
+        }
+
+        public GameScore CurrentScore { get; private set; }
+    }
+
+
+    [Subject(typeof(Game))]
+    public class when_create_a_new_tennis_game
+    {
+        Establish context = () =>
+            {
+                _game = new Game();
+            };
+
+        Because of = () => _game.Start(new GameScore(0, 0));
+
+        It should_have_empty_game_score = () => _game.CurrentScore.PlayerScore1.ShouldEqual(0);
+        static Game _game;
+    }
+
+    public class GameScore
+    {
+        public readonly int PlayerScore1;
+        public readonly int PlayerScore2;
+
+        public GameScore(int playerScore1, int playerScore2)
+        {
+            PlayerScore1 = playerScore1;
+            PlayerScore2 = playerScore2;
+        }
+    }
+}
