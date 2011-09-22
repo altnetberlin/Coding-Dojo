85e246ca2df16bc47201f4e48d301d660a44f22b
Janek Fellien <janek.fellien@googlemail.com>
2011-09-22 19:39:34 +0200

    Alle natürlichen Zahlen

---
+++ Kata FizzBuzz 22.09.2011/Team B/Kata.FizzBuzz/Kata.FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.FizzBuzz
+{
+	class FizzBuzzer
+	{
+		public static string GetResult(int number)
+		{
+			if (((number % 3) != 0) && ((number % 5) != 0))
+				return number.ToString();
+
+			if (number == 5)
+				return "Buzz";
+
+			if (number == 15)
+				return "FizzBuzz";
+
+			return "Fizz";
+		}
+	}
+}
---
+++ Kata FizzBuzz 22.09.2011/Team B/Kata.FizzBuzz/Kata.FizzBuzz/FizzBuzzer_Specs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace Kata.FizzBuzz
+{
+	[Subject("FizzBuzzer")]
+	public class when_number_div_3
+	{
+		Because of = () =>
+			{
+				result = FizzBuzzer.GetResult(3);
+
+			};
+
+		It should_returns_Fizz = () =>
+			{
+				result.ShouldEqual("Fizz");
+			};
+
+		static string result;
+	}
+
+	public class when_number_div_5
+	{
+		Because of = () =>
+		    result = FizzBuzzer.GetResult(5);
+
+		It should_return_Buzz = () =>
+			result.ShouldEqual("Buzz");
+
+		static string result;
+			
+	}
+
+	public class when_number_is_1
+	{
+		Because of = () =>
+					result = FizzBuzzer.GetResult(1);
+
+		It should_return_1 = () =>
+			result.ShouldEqual("1");
+
+		static string result;
+	}
+
+	public class when_number_is_15
+	{
+		Because of = () =>
+			result = FizzBuzzer.GetResult(15);
+
+		It should_return_1 = () =>
+			result.ShouldEqual("FizzBuzz");
+
+		static string result;
+	}
+
+	public class when_number_is_31
+	{
+		Because of = () =>
+			result = FizzBuzzer.GetResult(31);
+
+		It should_return_1 = () =>
+			result.ShouldEqual("31");
+
+		static string result;
+	}
+}
