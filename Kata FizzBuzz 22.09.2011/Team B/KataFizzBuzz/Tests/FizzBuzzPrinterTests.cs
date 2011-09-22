b0dd5b0330ec8d3232deec72d79c6a0433933baf
Marco Rasp <marco.rasp@gmail.com>
2011-09-22 19:47:17 +0200

    FizzBuzz

--- Kata FizzBuzz 22.09.2011/Team B/KataFizzBuzz/Tests/FizzBuzzPrinterTests.cs
+++ Kata FizzBuzz 22.09.2011/Team B/KataFizzBuzz/Tests/FizzBuzzPrinterTests.cs
@@ -5,6 +5,14 @@ namespace Tests
     [TestFixture]
     public class FizzBuzzPrinterTests
     {
+        FizzBuzzPrinter _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new FizzBuzzPrinter();
+        }
+
         [Test]
         public void FizzBuzzPrinterCanBeConstructed()
         {
@@ -14,9 +22,7 @@ namespace Tests
         [Test]
         public void When_FizzBuzzPrinter_Is_called_with_1_it_returns_1()
         {
-            var fizzbuzzer = new FizzBuzzPrinter();
-
-            var result = fizzbuzzer.Translate(1);
+            var result = _sut.Translate(1);
 
             Assert.AreEqual("1", result);
         }
@@ -24,9 +30,7 @@ namespace Tests
         [Test]
         public void When_Translate_is_called_with_3_it_returns_Fizz()
         {
-            var fizzbuzz = new FizzBuzzPrinter();
-
-            var result = fizzbuzz.Translate(3);
+            var result = _sut.Translate(3);
 
             Assert.AreEqual("Fizz", result);
         }
@@ -34,22 +38,34 @@ namespace Tests
         [Test]
         public void When_Translate_is_call_with_12_it_returns_Fizz()
         {
-            var fizzbuzz = new FizzBuzzPrinter();
-
-            var result = fizzbuzz.Translate(12);
+            var result = _sut.Translate(12);
 
             Assert.AreEqual("Fizz", result);
         }
+
         [Test]
         public void When_Translate_is_called_with_10_it_returns_Buzz()
         {
-            var fizzbuzz = new FizzBuzzPrinter();
-
-            var result = fizzbuzz.Translate(10);
+            var result = _sut.Translate(10);
 
             Assert.AreEqual("Buzz", result);
         }
 
+        [Test]
+        public void When_Translate_is_called_with_15_it_returns_FizzBuzz()
+        {
+            var result = _sut.Translate(15);
+
+            Assert.AreEqual("FizzBuzz",result);
+        }
+
+        [Test]
+        public void When_Translate_is_called_with_30_it_returns_FizzBuzz()
+        {
+            var result = _sut.Translate(30);
+
+            Assert.AreEqual("FizzBuzz",result);
+        }
     }
 
 
@@ -58,6 +74,9 @@ namespace Tests
     {
         public string Translate(int i)
         {
+            if (i % 3 == 0 && i % 5 == 0)
+                return "FizzBuzz";
+
             if (i % 3 == 0)
                 return "Fizz";
 
