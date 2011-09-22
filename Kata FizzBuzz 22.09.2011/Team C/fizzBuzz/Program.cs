708d56ef7b95fa8d982964a0b2a69b9cb8695cfb
malook <malook@ortec.org>
2011-09-22 19:32:25 +0200

    initial test for TDD explanation

---
+++ Kata FizzBuzz 22.09.2011/Team C/fizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class FizzBuzzer
+    {
+        public string Do()
+        {
+            return "";
+        }
+    }
+
+    class ProgramSpecs
+    {
+        private static FizzBuzzer sut;
+        private static string result;
+
+        // Arrange
+        Establish context = () => { sut = new FizzBuzzer(); };
+
+        // Act
+        private Because of = () => { result = sut.Do(); };
+
+        // Assert
+        private It should_return_empty_string = () => { result.ShouldEqual(""); };
+    }
+}
