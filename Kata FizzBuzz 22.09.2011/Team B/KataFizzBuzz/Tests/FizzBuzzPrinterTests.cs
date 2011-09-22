88154b63ef16562b01ffe1fb06c692ed9abc7398
Marco Rasp <marco.rasp@gmail.com>
2011-09-22 20:30:41 +0200

    Remove redundant test

--- Kata FizzBuzz 22.09.2011/Team B/KataFizzBuzz/Tests/FizzBuzzPrinterTests.cs
+++ Kata FizzBuzz 22.09.2011/Team B/KataFizzBuzz/Tests/FizzBuzzPrinterTests.cs
@@ -20,59 +20,11 @@ namespace Tests
         }
 
         [Test]
-        public void When_Translate_Is_called_with_1_it_returns_1()
-        {
-            var result = _sut.Translate(1);
-
-            Assert.AreEqual("1", result);
-        }
-
-        [Test]
-        public void When_Translate_Is_called_with_2_it_returns_2()
-        {
-            var result = _sut.Translate(2);
-
-            Assert.AreEqual("2", result);
-        }
-
-        [Test]
-        public void When_Translate_is_called_with_3_it_returns_Fizz()
-        {
-            var result = _sut.Translate(3);
-
-            Assert.AreEqual("Fizz", result);
-        }
-
-        [Test]
-        public void When_Translate_is_call_with_12_it_returns_Fizz()
-        {
-            var result = _sut.Translate(12);
-
-            Assert.AreEqual("Fizz", result);
-        }
-
-        [Test]
         public void When_Translate_is_called_with_10_it_returns_Buzz()
         {
             var result = _sut.Translate(10);
 
             Assert.AreEqual("Buzz", result);
-        }
-
-        [Test]
-        public void When_Translate_is_called_with_15_it_returns_FizzBuzz()
-        {
-            var result = _sut.Translate(15);
-
-            Assert.AreEqual("FizzBuzz", result);
-        }
-
-        [Test]
-        public void When_Translate_is_called_with_30_it_returns_FizzBuzz()
-        {
-            var result = _sut.Translate(30);
-
-            Assert.AreEqual("FizzBuzz", result);
         }
 
         [Test]
@@ -97,6 +49,8 @@ namespace Tests
         }
 
         [Test]
+        [TestCase(1, Result = "1")]
+        [TestCase(2, Result = "2")]
         [TestCase(7, Result = "7")]
         [TestCase(14, Result = "14")]
         [TestCase(23, Result = "23")]
