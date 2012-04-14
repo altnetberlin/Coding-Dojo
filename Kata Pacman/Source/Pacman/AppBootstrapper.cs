089d8218c902beeb7fb3599c78596aadd1551800
Christian Lang <christian_lang@gmx.de>
2012-04-14 17:57:39 +0200

    empty Caliburn.Micro-enabled project

---
+++ Kata Pacman/Source/Pacman/AppBootstrapper.cs
@@ -0,0 +1,39 @@
+﻿namespace Pacman
+{
+	using System;
+	using System.Collections.Generic;
+	using Caliburn.Micro;
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.Configuration;
+
+	public class AppBootstrapper : Bootstrapper<IShell>
+	{
+        private IUnityContainer container;
+
+        /// <summary>
+        /// Configure to use Unity. 
+        /// </summary>
+        protected override void Configure()
+        {
+            this.container = new UnityContainer()
+                .LoadConfiguration();
+
+            Logging.Configure();
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            return this.container.Resolve(serviceType, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return this.container.ResolveAll(serviceType);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            this.container.BuildUp(instance);
+        }
+	}
+}
---
+++ Kata Pacman/Source/Pacman/IShell.cs
@@ -0,0 +1,3 @@
+﻿namespace Pacman {
+    public interface IShell {}
+}
