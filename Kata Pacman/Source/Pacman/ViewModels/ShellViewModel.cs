98b3cfa79d2473e52e3e4e14200e897ff3e50f1c
Christian Lang <christian_lang@gmx.de>
2012-04-17 08:50:06 +0200

    created views and viewmodels with basic layout

---
+++ Kata Pacman/Source/Pacman/ViewModels/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Pacman.ViewModels {
+    using System.Windows;
+    using Caliburn.Micro;
+
+    public class ShellViewModel : Conductor<object>, IShell
+    {
+        public ShellViewModel()
+        {
+            ShowStart();
+        }
+
+        public void StartGame()
+        {
+            ActivateItem(new GameViewModel());
+        }
+
+        public void ShowStart()
+        {
+            ActivateItem(new StartViewModel());
+        }
+
+        public void Quit()
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void Continue()
+        {
+            ActivateItem(new StartViewModel());
+        }
+
+        public void ShowHighScore()
+        {
+            ActivateItem(new HighScoreViewModel());
+        }
+    }
+}
---
+++ Kata Pacman/Source/Pacman/ViewModels/StartViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pacman.ViewModels
+{
+    using Caliburn.Micro;
+
+    public class StartViewModel : Screen
+    {
+        public string PlayerName { get; set; }
+
+        protected IConductor Conductor
+        {
+            get { return (IConductor)Parent; }
+        }
+    }
+}
