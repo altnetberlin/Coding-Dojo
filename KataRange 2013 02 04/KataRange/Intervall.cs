3eebb9e932c5b714e30cf67e8f6a89eb9933acf2
unknown <Prsentation@.(none)>
2013-04-09 16:09:13 +0200

    added katas from 2013

---
+++ KataRange 2013 02 04/KataRange/Intervall.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataRange
+{
+
+    public class Interval
+    {
+        private readonly int _left;
+        private readonly int _right;
+
+        private readonly bool _isEmpty;
+
+        public Interval(int left, bool leftClosed, int right, bool rightClosed)
+        {
+            if (left == right && (!leftClosed || !rightClosed))
+            {
+                _isEmpty = true;
+            }
+            else
+            {
+                _left = left;
+                _right = right;
+                if (!leftClosed)
+                    _left++;
+                if (!rightClosed)
+                    _right--;
+            }
+        }
+
+        public bool Contains(List<int> ints)
+        {
+            return ints.All(Contains);
+        }
+
+        public bool Contains(int i)
+        {
+            if (_isEmpty)
+                return false;
+
+            return !(i > _right || i < _left);
+        }
+
+        public List<int> GetAllPoints()
+        {
+            var allpoints = new List<int> { };
+
+            if (!_isEmpty)
+            {
+                for (int i = _left; i <= _right; i++)
+                {
+                    allpoints.Add(i);
+                }
+            }
+
+            return allpoints;
+        }
+    }
+
+    [TestFixture]
+    public class IntervalTests
+    {
+        [TestCase(int.MaxValue, false, int.MaxValue, true, new int[] { 2 }, 10, Result = false)]
+        [TestCase(0, false, 0, false, new int[] { }, 11, Result = true)]
+
+        [TestCase(2, false, 2, false, new int[] { 2 }, 2, Result = false)]
+        [TestCase(2, false, 2, true, new int[] { 2 }, 3, Result = false)]
+        [TestCase(2, true, 2, false, new int[] { 2 }, 4, Result = false)]
+        [TestCase(2, true, 2, true, new int[] { 2 }, 5, Result = true)]
+
+        [TestCase(2, true, 6, false, new int[] { 1 }, 1, Result = false)]
+        [TestCase(2, true, 6, false, new int[] { 2, 6 }, 6, Result = false)]
+        [TestCase(2, true, 6, false, new int[] { 2, 4 }, 8, Result = true)]
+        [TestCase(2, true, 6, false, new int[] { 2, 7 }, 9, Result = false)]
+        [TestCase(2, false, 6, false, new int[] { 1 }, 10, Result = false)]
+        public bool IntervalContainsSet(int left, bool leftClosed, int right, bool rightClosed, int[] ints, int dummy)
+        {
+            var interval = new Interval(left, leftClosed, right, rightClosed);
+            var result = interval.Contains(ints.ToList());
+            return result;
+        }
+
+        [TestCase(2, true, 6, false, new int[] { 2, 3, 4, 5 }, 1)]
+        [TestCase(2, true, 5, false, new int[] { 2, 3, 4 }, 2)]
+        [TestCase(2, true, 2, false, new int[] { }, 3)]
+        public void GetAllPoints(int left, bool leftClosed, int right, bool rightClosed, int[] expected, int dummy)
+        {
+            var interval = new Interval(left, leftClosed, right, rightClosed);
+
+            var result = interval.GetAllPoints();
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ KataRange 2013 04 03/KataRangeTests/Class1Test.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KataRange;
+using NUnit.Framework;
+
+namespace KataRangeTests
+{
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void RangeContainsSingleElement()
+        {
+            // Arrange
+            var range = Range.Closed(2, 6);
+
+            // Act
+            var result = range.Contains(3);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void RangeDoesNotContainSingleElement()
+        {
+            // Arrange
+            var range = Range.Closed(2, 6);
+
+            // Act
+            var result = range.Contains(18);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestCase(2, 6, new[] { 3, 4 }, true)]
+        [TestCase(2, 6, new[] { 18, 3 }, false)]
+        [TestCase(2, 6, new int[0], true)]
+        [TestCase(int.MinValue, int.MaxValue, new[] { 3, 4 }, true)]
+        public void RangeContainsSet(int left, int right, int[] elements, bool expected)
+        {
+            // Arrange
+            var range = Range.Closed(left, right);
+
+            // Act
+            var result = range.Contains(elements);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestCase(2, 6, 2, false)]
+        [TestCase(2, 6, 3, true)]
+        [TestCase(2, 6, 6, false)]
+        [TestCase(int.MinValue, int.MinValue, 9782357, false)]
+        [TestCase(int.MaxValue, int.MaxValue, 9782357, false)]
+        [TestCase(2, 2, 2, false)]
+        public void OpenRangeContainsSingleElement(int left, int right, int element, bool expected)
+        {
+            // Arrange
+            var range = Range.Open(left, right);
+
+            // Act
+            var result = range.Contains(element);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(6, 0)]
+        [TestCase(int.MaxValue, int.MinValue)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void OpenRangeIsInvalid(int left, int right)
+        {
+            // Arrange
+            var range = Range.Open(left, right);
+        }
+
+        [Test]
+        [TestCase(6, 0)]
+        [TestCase(int.MaxValue, int.MinValue)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ClosedRangeIsInvalid(int left, int right)
+        {
+            // Arrange
+            var range = Range.Closed(left, right);
+        }
+
+        [Test]
+        [TestCase(6, 0)]
+        [TestCase(int.MaxValue, int.MinValue)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LeftOpenRangeIsInvalid(int left, int right)
+        {
+            // Arrange
+            var range = Range.LeftOpen(left, right);
+        }
+
+        [Test]
+        [TestCase(6, 0)]
+        [TestCase(int.MaxValue, int.MinValue)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RightOpenRangeIsInvalid(int left, int right)
+        {
+            // Arrange
+            var range = Range.RightOpen(left, right);
+        }
+
+        [TestCase(2, 6, 2, false)]
+        [TestCase(2, 6, 6, true)]
+        [TestCase(int.MinValue, int.MinValue, 6, false)]
+        [TestCase(2, 2, 2, false)]
+        public void LeftOpenRangeContainsSingleElement(int left, int right, int element, bool expected)
+        {
+            // Arrange
+            var range = Range.LeftOpen(left, right);
+
+            // Act
+            var result = range.Contains(element);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(2, 6, 2, true)]
+        [TestCase(2, 6, 6, false)]
+        [TestCase(int.MinValue, int.MinValue, 6, false)]
+        [TestCase(int.MaxValue, int.MaxValue, 6, false)]
+        [TestCase(2, 2, 2, false)]
+        public void RightOpenRangeContainsSingleElement(int left, int right, int element, bool expected)
+        {
+            // Arrange
+            var range = Range.RightOpen(left, right);
+
+            // Act
+            var result = range.Contains(element);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
