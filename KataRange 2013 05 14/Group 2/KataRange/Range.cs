f9e9db14a7d4c2228a5e55c0fb7edda1e2949bd9
Max Malook <community@malook.de>
2013-05-27 08:46:07 +0200

    kata range II group 2 (horizontal monitor)

---
+++ KataRange 2013 05 14/Group 2/KataRange/Range.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace KataRange
+{
+    public class Range
+    {
+        private readonly int _left;
+        private readonly int _right;
+
+        private Range(int left, int right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public bool Contains(int element)
+        {
+            return element >= _left && element <= _right;
+        }
+
+        public bool Contains(int[] elements)
+        {
+            return elements.All(Contains);
+        }
+
+        public static Range Closed(int left, int right)
+        {
+            if (right < left)
+                throw new ArgumentException("Right bla ...");
+
+            return GetRange(left, right, false, false);
+        }
+
+        public static Range Open(int left, int right)
+        {
+            if (right < left)
+                throw new ArgumentException("Right bla ...");
+
+            return GetRange(left, right, true, true);
+        }
+
+        private static Range GetRange(int left, int right, bool isLeftOpen, bool isRightOpen)
+        {
+            var l = isLeftOpen ? GetOpenLeft(left) : left;
+            var r = isRightOpen ? GetOpenRight(right) : right;
+
+            return new Range(l, r);
+        }
+
+        private static int GetOpenRight(int right)
+        {
+            var r = right;
+            if (right != int.MinValue)
+                r--;
+            return r;
+        }
+
+        private static int GetOpenLeft(int left)
+        {
+            var l = left;
+
+            if (left != int.MaxValue)
+                l++;
+            return l;
+        }
+
+        public static Range LeftOpen(int left, int right)
+        {
+            if (right < left)
+                throw new ArgumentException("Right bla ...");
+
+            return GetRange(left, right, true, false);
+        }
+
+        public static Range RightOpen(int left, int right)
+        {
+            if (right < left)
+                throw new ArgumentException("Right bla ...");
+
+            return GetRange(left, right, false, true);
+        }
+
+        public IEnumerable GetAllPoints()
+        {
+            for (int i = _left; i <= _right; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ KataRange 2013 05 14/Group 2/KataRangeTests/GetAllPointsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using KataRange;
+using NUnit.Framework;
+
+namespace KataRangeTests
+{
+    [TestFixture]
+    public class GetAllPointsTest
+    {
+        [TestCase(1, 1, new[] { 1 })]
+        [TestCase(1, 2, new[] { 1, 2 })]
+        public void ClosedRange(int left, int right, int[] expected)
+        {
+            var sut = Range.Closed(left, right);
+            var result = sut.GetAllPoints();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestCase(1, 1, new int[0])]
+        [TestCase(1, 3, new[] { 2 })]
+        public void OpenRange(int left, int right, int[] expected)
+        {
+            var sut = Range.Open(left, right);
+            var result = sut.GetAllPoints();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestCase(1, 3, new[] { 2, 3 })]
+        public void LeftOpenRange(int left, int right, int[] expected)
+        {
+            var sut = Range.LeftOpen(left, right);
+            var result = sut.GetAllPoints();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
