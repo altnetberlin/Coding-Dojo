36cfb04fc21ca7434db86788ec6e0225309a4acb
McO <marco.rasp@gmail.com>
2013-07-10 19:40:04 +0200

    When_Asking_For_07_2013

--- 2013 07 10/KataDojoCalendarTests/Specs.cs
+++ 2013 07 10/KataDojoCalendarTests/Specs.cs
@@ -1,4 +1,5 @@
-﻿using KataDojoCalendar;
+﻿using System;
+using KataDojoCalendar;
 using Machine.Specifications;
 
 namespace KataDojoCalendarTests
@@ -11,5 +12,18 @@ namespace KataDojoCalendarTests
         private Because of = () => { var calculator = new DojoCalendarCalculator(); };
 
         private It should_work = () => { };
+    }
+
+    [Subject(typeof (DojoCalendarCalculator))]
+    public class When_Asking_For_07_2013
+    {
+        Establish context = () => { sut = new DojoCalendarCalculator(); };
+
+        private Because of = () => { actual = sut.GetDateFor(7, 2013); };
+
+        private It should_return_10_07_2013 = () => actual.ShouldEqual(new DateTime(2013, 7, 10));
+
+        private static DojoCalendarCalculator sut;
+        private static DateTime actual;
     }
 }
--- 2013 07 10/UnknownKata/DojoCalendarCalculator.cs
+++ 2013 07 10/UnknownKata/DojoCalendarCalculator.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace KataDojoCalendar
 {
     public class DojoCalendarCalculator
     {
-        public DojoCalendarCalculator()
+        public DateTime GetDateFor(int month, int year)
         {
+            return new DateTime(year, month, 10);
         }
     }
 }
