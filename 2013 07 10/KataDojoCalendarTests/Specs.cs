4706616ce36248c5d7b0eb007cdfd05aa933ec40
McO <marco.rasp@gmail.com>
2013-07-10 20:26:41 +0200

    tests refactored

--- 2013 07 10/KataDojoCalendarTests/Specs.cs
+++ 2013 07 10/KataDojoCalendarTests/Specs.cs
@@ -14,56 +14,44 @@ namespace KataDojoCalendarTests
         private It should_work = () => { };
     }
 
-    [Subject(typeof (DojoCalendarCalculator))]
-    public class When_Asking_For_07_2013
+    public class WithDojoCalendarCalculator
     {
         Establish context = () => { sut = new DojoCalendarCalculator(); };
 
+        protected static DojoCalendarCalculator sut;
+        protected static DateTime actual;
+    }
+
+    [Subject(typeof (DojoCalendarCalculator))]
+    public class When_Asking_For_07_2013 : WithDojoCalendarCalculator
+    {
         private Because of = () => { actual = sut.GetDateFor(2013, 7); };
 
         private It should_return_10_07_2013 = () => actual.ShouldEqual(new DateTime(2013, 7, 10));
-
-        private static DojoCalendarCalculator sut;
-        private static DateTime actual;
     }
 
     [Subject(typeof(DojoCalendarCalculator))]
-    public class When_Asking_For_06_2013
+    public class When_Asking_For_06_2013 : WithDojoCalendarCalculator
     {
-        Establish context = () => { sut = new DojoCalendarCalculator(); };
-
         private Because of = () => { actual = sut.GetDateFor(2013, 6); };
 
         private It should_be_a_Tuesday = () => actual.DayOfWeek.ShouldEqual(DayOfWeek.Tuesday);
         private It should_be_in_June = () => actual.Month.ShouldEqual(6);
-
-        private static DojoCalendarCalculator sut;
-        private static DateTime actual;
     }
 
     [Subject(typeof(DojoCalendarCalculator))]
-    public class When_Asking_For_08_2013
+    public class When_Asking_For_08_2013 : WithDojoCalendarCalculator
     {
-        Establish context = () => { sut = new DojoCalendarCalculator(); };
-
         private Because of = () => { actual = sut.GetDateFor(2013, 8); };
 
         private It should_be_a_Thursday = () => actual.DayOfWeek.ShouldEqual(DayOfWeek.Thursday);
-
-        private static DojoCalendarCalculator sut;
-        private static DateTime actual;
     }
 
     [Subject(typeof(DojoCalendarCalculator))]
-    public class When_Asking_For_09_2013
+    public class When_Asking_For_09_2013 : WithDojoCalendarCalculator
     {
-        Establish context = () => { sut = new DojoCalendarCalculator(); };
-
         private Because of = () => { actual = sut.GetDateFor(2013, 9); };
 
         private It should_be_a_Monday = () => actual.DayOfWeek.ShouldEqual(DayOfWeek.Monday);
-
-        private static DojoCalendarCalculator sut;
-        private static DateTime actual;
     }
 }
