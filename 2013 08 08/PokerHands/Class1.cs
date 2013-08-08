fe5d8f076633184ca4113e60c05dc32427ecdcbd
McO <marco.rasp@gmail.com>
2013-08-08 21:01:41 +0200

    complete hand rank

--- 2013 08 08/PokerHands/Class1.cs
+++ 2013 08 08/PokerHands/Class1.cs
@@ -144,6 +144,82 @@ namespace PokerHands
             //Act
             //Assert
             Assert.AreNotEqual(Hand.HandRank.ThreeOfAKind, hand.Rank);
+            Assert.AreEqual(Hand.HandRank.FullHouse, hand.Rank);
+        }
+
+        [Test]
+        public void RankOfStraightIsStraight()
+        {
+            //Arrange
+            var hand = new Hand(
+                new Card(Card.CardValue.Two, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Three, Card.CardSuit.Hearts),
+                new Card(Card.CardValue.Four, Card.CardSuit.Spades),
+                new Card(Card.CardValue.Five, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Six, Card.CardSuit.Hearts));
+            //Act
+            //Assert
+            Assert.AreEqual(Hand.HandRank.Straight, hand.Rank);
+        }
+
+        [Test]
+        public void RankOfHighCardIsNotStraight()
+        {
+            //Arrange
+            var hand = new Hand(
+                new Card(Card.CardValue.Two, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Three, Card.CardSuit.Hearts),
+                new Card(Card.CardValue.Four, Card.CardSuit.Spades),
+                new Card(Card.CardValue.Five, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Seven, Card.CardSuit.Hearts));
+            //Act
+            //Assert
+            Assert.AreNotEqual(Hand.HandRank.Straight, hand.Rank);
+        }
+
+        [Test]
+        public void RankOfStraightFlushIsNotStraight()
+        {
+            //Arrange
+            var hand = new Hand(
+                new Card(Card.CardValue.Two, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Three, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Four, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Five, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Six, Card.CardSuit.Clubs));
+            //Act
+            //Assert
+            Assert.AreEqual(Hand.HandRank.StraightFlush, hand.Rank);
+        }
+
+        [Test]
+        public void RankOfFlushIsNotStraight()
+        {
+            //Arrange
+            var hand = new Hand(
+                new Card(Card.CardValue.Two, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Three, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Four, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Five, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Jack, Card.CardSuit.Clubs));
+            //Act
+            //Assert
+            Assert.AreEqual(Hand.HandRank.Flush, hand.Rank);
+        }
+
+        [Test]
+        public void RankOfFourOfAKindIsNotStraight()
+        {
+            //Arrange
+            var hand = new Hand(
+                new Card(Card.CardValue.Two, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Queen, Card.CardSuit.Hearts),
+                new Card(Card.CardValue.Queen, Card.CardSuit.Spades),
+                new Card(Card.CardValue.Queen, Card.CardSuit.Clubs),
+                new Card(Card.CardValue.Queen, Card.CardSuit.Diamonds));
+            //Act
+            //Assert
+            Assert.AreEqual(Hand.HandRank.FourOfAKind, hand.Rank);
         }
     }
 
@@ -154,7 +230,12 @@ namespace PokerHands
             HighCard,
             TwoPairs,
             ThreeOfAKind,
-            Pair
+            Pair,
+            Straight,
+            StraightFlush,
+            FullHouse,
+            Flush,
+            FourOfAKind
         }
 
         private readonly Card[] _cards;
@@ -168,15 +249,56 @@ namespace PokerHands
 
         private HandRank GetRank()
         {
-            var groups = _cards.GroupBy(card => card.Value);
-            if ((groups.Count(grouping => grouping.Count() == 3) == 1) &&
-                (groups.Count() == 3))
+            var suitGroups = _cards.GroupBy(card => card.Suit);
+            var valueGroups = _cards.GroupBy(card => card.Value);
+            if (HasStraight(valueGroups) && HasFlush(suitGroups))
+                return HandRank.StraightFlush;
+            if (IsFourOfAKind(valueGroups))
+                return HandRank.FourOfAKind;
+            if (HasThreeOfAKind(valueGroups) && HasPair(valueGroups))
+                return HandRank.FullHouse;
+            if (HasFlush(suitGroups))
+                return HandRank.Flush;
+            if (HasStraight(valueGroups))
+                return HandRank.Straight;
+            if (HasThreeOfAKind(valueGroups))
                 return HandRank.ThreeOfAKind;
-            if (groups.Count(grouping => grouping.Count() == 2) == 2)
+            if (IsTwoPairs(valueGroups))
                 return HandRank.TwoPairs;
-            if (groups.Count() == 4)
+            if (HasPair(valueGroups))
                 return HandRank.Pair;
             return HandRank.HighCard;
+        }
+
+        private bool IsFourOfAKind(IEnumerable<IGrouping<Card.CardValue, Card>> valueGroups)
+        {
+            return valueGroups.Count(grouping => grouping.Count() == 4) == 1;
+        }
+
+        private static bool HasPair(IEnumerable<IGrouping<Card.CardValue, Card>> valueGroups)
+        {
+            return valueGroups.Count(grouping => grouping.Count() == 2) == 1;
+        }
+
+        private static bool IsTwoPairs(IEnumerable<IGrouping<Card.CardValue, Card>> valueGroups)
+        {
+            return valueGroups.Count(grouping => grouping.Count() == 2) == 2;
+        }
+
+        private static bool HasThreeOfAKind(IEnumerable<IGrouping<Card.CardValue, Card>> valueGroups)
+        {
+            return valueGroups.Count(grouping => grouping.Count() == 3) == 1;
+        }
+
+        private static bool HasFlush(IEnumerable<IGrouping<Card.CardSuit, Card>> suitGroups)
+        {
+            return suitGroups.Count() == 1;
+        }
+
+        private bool HasStraight(IEnumerable<IGrouping<Card.CardValue, Card>> valueGroups)
+        {
+            return valueGroups.Count()==5 && 
+                   (_cards.Max(x=>x.Value) - _cards.Min(x=>x.Value) == 4);
         }
 
         public Card.CardValue GetHighCard()
@@ -193,10 +315,14 @@ namespace PokerHands
             Three,
             Four,
             Five,
+            Six,
+            Seven,
             Eight,
             Nine,
             King,
-            Ace
+            Ace,
+            Jack,
+            Queen
         }
 
         public enum CardSuit
