b3bf209f88b0f6099f3db3d0e793092d9296741f
McO <marco.rasp@gmail.com>
2013-09-09 19:50:37 +0200

    first test for functional tennis

---
+++ 2013 09 09/FunctionalTennis/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FunctionalTennis
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void T()
+        {
+            Assert.AreEqual(Tuple.Create(0, 0), Game.Start());
+        }
+    }
+
+    public class Game
+    {
+        public static object Start()
+        {
+            return Tuple.Create(0, 0);
+        }
+    }
+}
