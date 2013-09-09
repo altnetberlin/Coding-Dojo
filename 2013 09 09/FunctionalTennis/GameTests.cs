658115a1e94a24caf73f8cbaea0317e8d2434285
McO <marco.rasp@gmail.com>
2013-09-09 20:55:31 +0200

    THE END

--- 2013 09 09/FunctionalTennis/GameTests.cs
+++ 2013 09 09/FunctionalTennis/GameTests.cs
@@ -4,45 +4,84 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 
+using GameState = System.Tuple<int, int>;
+
 namespace FunctionalTennis
 {
     [TestFixture]
     public class GameTests
     {
         [Test]
-        public void GameStartsLoveAll()
+        public void GameStartsLoveAllFunctional()
         {
-            Assert.AreEqual(Tuple.Create(0, 0), Game.Start());
+            AssertCorrectState(Tuple.Create(0, 0), new Func<GameState, GameState>[0]);
         }
 
         [Test]
-        public void WhenLoveAllAndPlayerOneScoresShouldBeFifteenLove()
+        public void WhenLoveAllAndPlayerOneScoresShouldBeFifteenLoveFunctional()
         {
-            Assert.AreEqual(Tuple.Create(15, 0), Game.PlayerOneScores(Tuple.Create(0, 0)));
+            AssertCorrectState(Tuple.Create(15, 0), new Func<GameState, GameState>[] { Game.PlayerOneScores });
         }
 
         [Test]
-        public void WhenLoveAllAndPlayerTwoScoresShouldBeLoveFifteen()
+        public void WhenLoveAllAndPlayerTwoScoresShouldBeFifteenLoveFunctional()
         {
-            Assert.AreEqual(Tuple.Create(0, 15), Game.PlayerTwoScores(Tuple.Create(0, 0)));
+            AssertCorrectState(Tuple.Create(0, 15), new Func<GameState, GameState>[] { Game.PlayerTwoScores });
+        }
+
+        [Test]
+        public void WhenLoveAllAndPlayerOneScoresShouldBeThirtyLoveFunctional()
+        {
+            AssertCorrectState(Tuple.Create(30, 0), new Func<GameState, GameState>[]
+                                                        {
+                                                            Game.PlayerOneScores, 
+                                                            Game.PlayerOneScores,
+                                                        });
+        }
+
+        //[Test]
+        //public void WhenLoveAllAndPlayerOneScoresShouldBeFortyLoveFunctional()
+        //{
+        //    AssertCorrectState(Tuple.Create(40, 0), new Func<GameState, GameState>[]
+        //                                                {
+        //                                                    Game.PlayerOneScores, 
+        //                                                    Game.PlayerOneScores,
+        //                                                    Game.PlayerOneScores,
+        //                                                });
+        //}
+
+        private void AssertCorrectState(GameState expectedState, IEnumerable<Func<GameState, GameState>> actions)
+        {
+            var state = Game.State(actions);
+            Assert.AreEqual(expectedState, state);
         }
     }
 
     public class Game
     {
-        public static object Start()
+        public static GameState PlayerOneScores(GameState gameState)
         {
-            return Tuple.Create(0, 0);
+            return Tuple.Create(gameState.Item1 + 15, 0);
         }
 
-        public static object PlayerOneScores(Tuple<int, int> gameState)
-        {
-            return Tuple.Create(15, 0);
-        }
-
-        public static object PlayerTwoScores(Tuple<int, int> gameState)
+        public static GameState PlayerTwoScores(GameState gameState)
         {
             return Tuple.Create(0, 15);
+        }
+
+        public static GameState State(IEnumerable<Func<GameState, GameState>> actions)
+        {
+            return X(Tuple.Create(0, 0), actions);
+        }
+
+        static GameState X(GameState a, IEnumerable<Func<GameState, GameState>> list)
+        {
+            if (!list.Any())
+                return a;
+
+            var f = list.First();
+            var rest = list.Skip(1);
+            return X(f(a), rest);
         }
     }
 }
